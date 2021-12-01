f20bc6e0159c9486b53c1e5ae4d2d858659551d9
Vinicius Lopes <viniloppes2000@gmail.com>
2021-12-01 00:39:12 -0300

    Criando login

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginComponent = void 0;
+var LoginComponent = /** @class */ (function () {
+    function LoginComponent() {
+    }
+    return LoginComponent;
+}());
+exports.LoginComponent = LoginComponent;
+//# sourceMappingURL=login.component.js.map
